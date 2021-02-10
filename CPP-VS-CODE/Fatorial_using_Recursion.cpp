6c3902059eb2f1e0ed93104363d989641950a242
Kirti Sharma <59800818+kirti2602@users.noreply.github.com>
2021-02-10 15:31:44 +0530

    Add files via upload

---
+++ CPP-VS-CODE/Fatorial_using_Recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+cout<<"calculating factorial of "<<n<<endl;
+if (n==0)
+    return 1;
+else{
+    int smallOutput = n*factorial(n-1);
+     cout<<"factorial of "<<n<<"= "<<smallOutput<<endl;
+    return smallOutput;
+   
+}
+
+    }
+
+
+int main(){
+clrscr();
+int n;
+cin>>n;
+int output = factorial(n);
+cout<<"Factorial of "<<n<<" is "<<output<<endl;
+
+return 0;
+}
